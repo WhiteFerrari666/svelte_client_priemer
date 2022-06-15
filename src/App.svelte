0028d281ac10961345a4467f7e9aa072e15f6803
Martin <der_moaddin@yahoo.de>
2022-06-15 20:45:41 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    import MyFirstComponent from "./components/MyFirstComponent.svelte";
+    import Button from '@smui/button';
+    import Extended from '@smui/button';
+    import Fab, {Label, Icon} from '@smui/fab';
+
+    export let name: string;
+    let clicked = 0;
+</script>
+
+
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <MyFirstComponent/>
+    <Button/>
+    <Extended/>
+</main>
+
+<Fab on:click={() => clicked++} extended>
+    <Icon class="material-icons">favorite</Icon>
+    <Label>Extended</Label>
+</Fab>
+<Fab color="primary" on:click={() => clicked++} extended>
+    <Icon class="material-icons">favorite</Icon>
+    <Label>Extended</Label>
+</Fab>
+<div class="margins">
+    <Fab on:click={() => clicked++} extended>
+        <Label>Extended W/o Icon</Label>
+    </Fab>
+</div>
+<div class="margins">
+    <Fab color="primary" on:click={() => clicked++} extended>
+        <Label>Extended W/o Icon</Label>
+    </Fab>
+</div>
+
+<pre class="status">Clicked: {clicked}</pre>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ src/components/MyFirstComponent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    async function helloWorld() {
+        // Diese Funktion tut aktuell nichts Sinnvolles
+        const res = await fetch('https://api.github.com/users');
+        const users = await res.json();
+
+        console.log('the users', users);
+
+        if (res.ok) {
+            return users;
+        } else {
+            throw new Error(users);
+        }
+    }
+
+    helloWorld();
+</script>
+
+
+<main>
+    <div>HELLO </div>
+</main>
