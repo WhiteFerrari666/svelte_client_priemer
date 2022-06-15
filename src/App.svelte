9cdea5437c6f782fe3052653eb4de1325a59f71f
Martin <der_moaddin@yahoo.de>
2022-06-15 21:50:28 +0200

    Navigator-Komponente ganz rudimentär aufgenommen
    sieht scheiße aus, aber das Prinzip zählt erstmal

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,48 @@
     import Extended from '@smui/button';
     import Fab, {Label, Icon} from '@smui/fab';
 
+    import { Router, Route, Link } from "svelte-navigator";
+    import Blog from "./components/Blog.svelte";
+    import BackButton from "./components/BackButton.svelte";
+    import ForwardButton from "./components/ForwardButton.svelte";
+
     export let name: string;
     let clicked = 0;
 </script>
+
+<Router>
+    <header>
+        <h1>History</h1>
+
+        <nav>
+            <BackButton />
+            <ForwardButton />
+            <Link to="/">Base</Link>
+            <Link to="home">Home</Link>
+            <Link to="about">About</Link>
+            <Link to="blog">Blog</Link>
+        </nav>
+    </header>
+
+    <main>
+        <Route path="blog/*blogRoute" component={Blog} />
+
+        <Route path="home">
+            <h3>Home</h3>
+            <p>Home sweet home...</p>
+        </Route>
+
+        <Route path="about">
+            <h3>About</h3>
+            <p>That's what it's all about!</p>
+        </Route>
+
+        <Route>
+            <h3>Default</h3>
+            <p>No Route could be matched.</p>
+        </Route>
+    </main>
+</Router>
 
 
 <main>
