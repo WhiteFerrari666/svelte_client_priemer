42a769e636c177cf6166cb4c8f42dab81cdb021e
Martin <der_moaddin@yahoo.de>
2022-06-25 23:06:08 +0200

    Stores überarbeitet

--- scripts/store.ts
+++ scripts/store.ts
@@ -2,11 +2,27 @@ import {readable, writable} from "svelte/store";
 
 // HowTo siehe hier: https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_stores
 
-export const user = writable("", (value) => console.log("user " + value.name + ""));
-
-
 // Bei Bedarf kann hier die URL des lokalen FAP-Servers angepasst werden,
 // bspw. wenn er bei euch auf einem anderen Port läuft als hier angegeben.
-export const fapServerBaseURL = readable(null, set => {
-    set("http://localhost:8088/FAPServer/service/fapservice")
-});
+export const fapServerBaseURL = readable("http://localhost:8088/FAPServer/service/fapservice");
+
+
+// eingeloggter User
+export const user =
+    writable("", () => {
+        return () => console.log("user cleared")
+    });
+
+user.subscribe(value => {
+    console.log("user set: " + value);
+})
+
+// aktuelle SessionID
+export const currentSession =
+    writable("", () => {
+        return () => console.log("session cleared");
+    });
+
+currentSession.subscribe(value => {
+    console.log("new session set: " + value);
+})
