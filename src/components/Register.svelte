325ebbd20dff47f8ca0e15236733d031b5585590
Martin <der_moaddin@yahoo.de>
2022-06-25 20:50:42 +0200

    cleanup

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -14,7 +14,6 @@
     let open = false;
 
     let feedbackSnackbar: SnackbarComponentDev;
-    let snackbarWithoutClose: SnackbarComponentDev;
     let snackbarText = "";
 
     let successHintVisible = false;
