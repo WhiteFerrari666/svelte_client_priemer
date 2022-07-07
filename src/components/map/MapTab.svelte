526217166f03ddc74274ac86361d18d345784fef
Martin <der_moaddin@yahoo.de>
2022-07-07 17:43:27 +0200

    Marker für Adresse setzen

--- src/components/map/MapTab.svelte
+++ src/components/map/MapTab.svelte
@@ -3,16 +3,30 @@
     import Button from "@smui/button";
     import {Icon, Label} from "@smui/fab";
     import Dialog, {Content, Title} from "@smui/dialog";
-    import {Actions} from "@smui/snackbar";
+    import Actions from "@smui/dialog/src/Actions";
     import {Marker} from "maplibre-gl";
     import {mapComponent} from '../../../scripts/stores';
+    import Textfield from "@smui/textfield";
+    import {getKoordinatenFuerAdresse, getOrtForPlz} from "../../service/FapServerQueryService";
 
     // Hilfsvariable für Daten-Dialog
     let open = false;
 
-    function addMarker(lng: number, lat: number) {
+    let plz = "";
+    let ort = "";
+    let strasse = "";
+
+    async function getOrtsnameFromFapServer(plz: string) {
+        ort = await getOrtForPlz(plz);
+    }
+
+    async function addMarker() {
+        let result = await getKoordinatenFuerAdresse(plz, ort, strasse);
+
+        let lngNumber: number = +result.laengengrad;
+        let latNumber: number = +result.breitengrad;
         new Marker({color: getRandomColor()})
-            .setLngLat([lng, lat])
+            .setLngLat([lngNumber, latNumber])
             .addTo($mapComponent);
     }
 
@@ -28,20 +42,34 @@
 </script>
 
 <Button variant="raised" on:click={() => (open = true)}>
-    <Label>Neuen Marker erfassen</Label>
+    <Label>Neuer Marker</Label>
 </Button>
-<Dialog bind:open aria-labelledby="marker-title" aria-describedby="simple-content">
+
+<Dialog bind:open aria-labelledby="marker-title" aria-describedby="marker-content">
     <Title id="marker-title">Daten erfassen</Title>
     <Content id="marker-content">
-        <Actions>
-            <Button>
-                <Label>Abbrechen</Label>
-            </Button>
-            <Button variant="raised" on:click={() => addMarker(7.6261347, 51.9606649)}>
-                <Label>Marker setzen</Label>
-                <Icon class="material-icons">push_pin</Icon>
-            </Button>
-        </Actions>
+        <div class="marker-data">
+            <div>
+                <Textfield bind:value={plz} label="PLZ" on:focusout={() => getOrtsnameFromFapServer(plz)} required/>
+            </div>
+            <div>
+                <Textfield bind:value={ort} label="Ort" required/>
+            </div>
+            <div>
+                <Textfield bind:value={strasse} label="Strasse" required/>
+            </div>
+        </div>
     </Content>
+
+    <Actions>
+        <Button>
+            <Label>Abbrechen</Label>
+        </Button>
+        <Button variant="raised" on:click={() => addMarker()}>
+            <Label>Marker setzen</Label>
+            <Icon class="material-icons">push_pin</Icon>
+        </Button>
+    </Actions>
 </Dialog>
+<br>
 <MapLibreMap/>
